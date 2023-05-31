169d462e9f8270608251e51ccc5ec71e984258ae
panhq <hypofiasco@gmail.com>
2023-05-31 16:02:49 +0800

    shell

--- CS/others/shell.sh
+++ CS/others/shell.sh
@@ -8,3 +8,10 @@ lsof -i tcp:8000 | grep Python | awk '{print $2}' | xargs kill -9  # 关闭所�
 
 # http server
 python3 -m http.server 8000 --bind 127.0.0.1
+
+# http test
+max=10
+for i in `seq 1 $max`
+do
+    echo `curl -o /dev/null -s -w %{http_code}:%{time_namelookup}:%{time_connect}:%{time_appconnect}:%{time_pretransfer}:%{time_starttransfer}:%{time_total} https://sf.jmc.com.cn:8081/`
+done
