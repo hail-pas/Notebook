203bc5174c03153572976665ab1318d04d562b40
panhq <hypofiasco@gmail.com>
2023-05-31 16:02:06 +0800

    shell

--- CS/others/shell.sh
+++ CS/others/shell.sh
@@ -4,6 +4,7 @@ ssh -L 0.0.0.0:8000:127.0.0.1:8000 root@10.211.55.3
 # lsof
 lsof -i -n | grep ssh  # 查看ssh隧道
 lsof -i tcp:3000  # 查看端口占用
+lsof -i tcp:8000 | grep Python | awk '{print $2}' | xargs kill -9  # 关闭所有占用8000的程序
 
 # http server
 python3 -m http.server 8000 --bind 127.0.0.1
